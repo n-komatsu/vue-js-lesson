8df619273bf76e6a01f51b1d436090ba5e712e01
n-komatsu <komatsu@gizumo-inc.jp>
2019-03-12 11:45:56 +0900

    lesson4のサンプル実装

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1>Hello Vue</h1>
+  <h1>{{message}}</h1>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello Vue',
+    }
+  }
+}
+</script>
+
+<style scoped>
+  h1 {
+    color: green;
+  }
+</style>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import MyApp from './Components/App';
 
 new Vue({
   el: '#app',
-  //importしたオブジェクトを渡す
   components: {
     MyApp
   },
